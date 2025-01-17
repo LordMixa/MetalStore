2ae3fb9a84fd4d1a5cb082eaff0dde2d7504414b
Misha Vasilkov <mishytka.vasilkov@gmail.com>
2025-01-17 16:23:29 +0200

    #2 - clothingItem repository, configuration added;

---
+++ MetalStore.Data/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using MetalStore.Data.Repositories;
+using MetalStore.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MetalStore.Data;
+
+public static class Configuration
+{
+    public static void Configure(IServiceCollection serviceCollection, string connectionString)
+    {
+        serviceCollection.AddTransient<IClothingItemRepository, ClothingItemRepository>();
+
+        serviceCollection.AddDbContext<MetalStoreContext>(options =>
+            options.UseSqlServer(connectionString));
+    }
+}
---
+++ MetalStore.Data/MetalStoreContext.cs
@@ -0,0 +1,13 @@
+﻿using MetalStore.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MetalStore.Data;
+
+public class MetalStoreContext : DbContext
+{
+    public MetalStoreContext(DbContextOptions<MetalStoreContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ClothingItem> ClothingItems { get; set; }
+}
